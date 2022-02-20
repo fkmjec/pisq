9496ba2cf8d11446fcd0696660e605f4907f0812
František Kmječ <frantisek.kmjec@gmail.com>
2022-02-20 14:09:29 +0100

    Remove error handling code as it gets handled anyway by ErrorView

--- lib/pisq_web/live/user_live.ex
+++ lib/pisq_web/live/user_live.ex
@@ -18,16 +18,11 @@ defmodule PisqWeb.Live.UserLive do
 
   def mount(params, _session, socket) do
     id = params["id"]
-
-    case GameUtils.game_exists?(id) do
-    true -> game_state = GameUtils.get_game_state(id)
-      socket = assign(socket, :id, id)
-      |> assign(game_state)
-      Endpoint.subscribe(id)
-      {:ok, socket}
-    _ ->
-      {:ok, render(socket, to: "/")} # FIXME here should be a proper error message. I need to look up how that is done
-    end
+    game_state = GameUtils.get_game_state(id)
+    socket = assign(socket, :id, id)
+    |> assign(game_state)
+    Endpoint.subscribe(id)
+    {:ok, socket}
   end
 
   def handle_info(%{event: "board_update", payload: state}, socket) do
