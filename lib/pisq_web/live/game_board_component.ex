d6b03f33677ed71ffa5bbdd89c5ee75749ff636d
František Kmječ <frantisek.kmjec@gmail.com>
2022-06-22 17:03:32 +0200

    Enhance the looks of crosses and circles

--- lib/pisq_web/live/game_board_component.ex
+++ lib/pisq_web/live/game_board_component.ex
@@ -13,10 +13,10 @@ defmodule PisqWeb.Live.GameBoardComponent do
               phx-click="place_symbol"
               phx-value-x="<%= x %>"
               phx-value-y="<%= y %>">
-              <%= raw get_symbol_to_display(@board, x, y) %>
+              <span class="symbol"><%= raw get_symbol_to_display(@board, x, y) %></span>
               </a>
               <span class="taken-field <%= hide_when(@board[{x, y}] == nil) %>">
-              <%= raw get_symbol_to_display(@board, x, y) %>
+              <span class="symbol"><%= raw get_symbol_to_display(@board, x, y) %></span>
               </span>
             </td>
           <% end %>
@@ -30,8 +30,30 @@ defmodule PisqWeb.Live.GameBoardComponent do
   defp get_symbol_to_display(board, x, y) do
     case board[{x, y}] do
       nil -> "&nbsp;"
-      :crosses -> "x"
-      :circles -> "o"
+      :crosses -> """
+      <svg
+        version="1.1"
+        id="Capa_1"
+        x="0px"
+        y="0px"
+        viewBox="0 0 20 20.000002"
+        xml:space="preserve"
+        width="20"
+        height="20.000002"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:svg="http://www.w3.org/2000/svg"><defs
+        id="defs1024" />
+      <polygon
+        points="0.708,457.678 33.149,490 245,277.443 456.851,490 489.292,457.678 277.331,245.004 489.292,32.337 456.851,0 245,212.564 33.149,0 0.708,32.337 212.669,245.004 "
+        id="polygon989"
+        transform="matrix(0.04093462,0,0,0.04081633,-0.02898171,0)" />
+      </svg>
+      """
+      :circles -> """
+      <svg height="30" width="30">
+      <circle cx="15" cy="11" r="10" stroke="black" stroke-width="2" fill="white" />
+      </svg>
+      """
       _ -> "something went wrong"
     end
   end
